c0dfc3b3cc8b6f791b556e9e20c003f3ac1e66b2
OmkarMayekar <mayekarom27@gmail.com>
2021-10-18 13:09:42 +0530

    First Commit

---
+++ src/main/java/com/example/demo/TracktradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracktradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracktradeApplication.class, args);
+	}
+
+}
